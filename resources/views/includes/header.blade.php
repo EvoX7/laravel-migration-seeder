f3f0c4c8939afe16ceff2572158a0fcd5b7719a8
EvoX7 <creativesoultouch@gmail.com>
2022-09-09 15:02:01 +0200

    Init

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,25 @@
+<header>
+    <nav class="navbar-dark container navbar navbar-expand-lg mb-5">
+        <div class="container">
+            <a href="#"><img id="logo" src="{{ asset('/assets/img/btlogo.png') }}"
+                    alt="logo"></a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse ms-4 d-flex justify-content-between" id="navbarNavAltMarkup">
+                <div class="navbar-nav fw-bold">
+                    <a class="nav-link text-white" aria-current="page" href="#">Book a ticket</a>
+                </div>
+                <div class="navbar-nav fw-bold">
+                    <a class="nav-link text-white me-3" href="#">Login</a>
+
+                    <form class="form-inline my-2 my-lg-0">
+                        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </nav>
+</header>
