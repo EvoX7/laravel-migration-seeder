b43f5fc593859f03f42991d3489ca21c5290a692
EvoX7 <creativesoultouch@gmail.com>
2022-09-09 19:08:04 +0200

    fix - tables fixed

--- resources/views/includes/trains_table.blade.php
+++ resources/views/includes/trains_table.blade.php
@@ -1,38 +1,44 @@
 
-<div class="container">
-
-
+<div class="container mt-5">
 @forelse ($trains as $train)
-<table class="text-center mb-3 mt-5">
-  <tr>
-    <th>Company</th>
-    <th>Departure Station</th>
-    <th>Arrival Station</th>
-    <th>Departure Time</th>
-    <th>Arrival Time</th>
-    <th>Train Code</th>
-    <th>Number Of Coaches</th>
-    <th>On Time</th>
-    <th>Cancelled</th>
-   
-
+<table class="table table-dark table-striped mt-3">
+        <thead>
+          <tr>
+            <th scope="col">Company</th>
+            <th scope="col">Departure Station</th>
+            <th scope="col">Arrival Station</th>
+            <th scope="col">Departure Time</th>
+            <th scope="col">Arrival Time</th>
+            <th scope="col">Train Code</th>
+            <th scope="col">Number Of Coaches</th>
+            <th scope="col">On Time</th>
+            <th scope="col">Cancelled</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{{ $train->company }}</td>
+            <td>{{ $train->departure_station }}</td>
+            <td>{{ $train->arrival_station }}</td>
+            <td>{{ $train->departure_time }}</td>
+            <td>{{ $train->arrival_time }}</td>
+            <td>{{ $train->train_code }}</td>
+            <td>{{ $train->number_of_coaches }}</td>
+            <td>{{ $train->is_on_time }}</td>
+            <td>{{ $train->cancelled }}</td>
+          </tr>
+        </tbody>
+      </table>
     
-  </tr>
-  <tr>
-    <td>{{ $train->company }}</td>
-    <td>{{ $train->departure_station }}</td>
-    <td>{{ $train->arrival_station }}</td>
-    <td>{{ $train->departure_time }}</td>
-    <td>{{ $train->arrival_time  }}</td>
-    <td>{{ $train->train_code }}</td>
-    <td>{{ $train->number_of_coaches }}</td>
-    <td>{{ $train->is_on_time }}</td>
-    <td>{{ $train->cancelled }}</td>
-  </tr>
 
-</table>
-@empty
+      @empty
    <h3 class="fs-1 text-center mt-5">No Trains available</h3>
  @endforelse
-
 </div>
+
+
+
+    
+
+
+
